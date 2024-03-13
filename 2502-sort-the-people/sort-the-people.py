0e346243a813382cc3ef727ff7039c9fbdbad124
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-03-13 23:55:56 +0300

    Time: 102 ms (71.71%) | Memory: 17.1 MB (33.91%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,7 +1,8 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         #decending is from Top -> Bottom        
-        sorted_people= sorted(list(zip(names, heights)), reverse=True, key= lambda person: person[1])
+        sorted_people= sorted(list(zip(names, heights)), reverse=True, key= lambda heights: heights[1])
+        print(sorted_people)
         # unpack to get the name and height after sorting.
         name , height =zip(*sorted_people) 
         return name
