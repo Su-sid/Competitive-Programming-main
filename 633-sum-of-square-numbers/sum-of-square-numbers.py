6d0d53a5f0409a25c73c28a5bee71b3497a6c427
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-03-18 19:23:22 +0300

    Time: 88 ms (73.90%) | Memory: 16.6 MB (51.72%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.py
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -24,7 +24,10 @@ class Solution:
         l, r = 0, int(c ** 0.5)
         while l <= r:
             lhs = l*l + r*r
-            if lhs == c: return True
-            if lhs < c: l += 1
-            else: r -= 1
+            if lhs == c: 
+                return True
+            if lhs < c: 
+                l += 1
+            else: 
+                r -= 1
         return False
