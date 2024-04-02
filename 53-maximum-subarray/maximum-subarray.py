21fc89ef7b90c490c60f2145f2621146c29d05db
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-04-02 21:28:06 +0300

    Time: 466 ms (99.65%) | Memory: 31 MB (63.66%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,10 +1,24 @@
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+#         current= final= nums[0]
+#         for i in range(1, len(nums)):
+#             #kadanes algorithm
+#             current= max(nums[i], nums[i]+current)
+#             if current > final:
+#                 final = current
+#         return final
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        current= final= nums[0]
-        for i in range(1, len(nums)):
-            #kadanes algorithm
-            current= max(nums[i], nums[i]+current)
-            if current > final:
-                final = current
-        return final
+        maxSum = float('-inf')
+        currentSum = 0
         
+        for num in nums:
+            currentSum += num
+            
+            if currentSum > maxSum:
+                maxSum = currentSum
+            
+            if currentSum < 0:
+                currentSum = 0
+        
+        return maxSum
