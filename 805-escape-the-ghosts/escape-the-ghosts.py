7aa65e3bfcb5583417ca2a46af9a737362189923
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-04-04 12:01:10 +0300

    Time: 40 ms (95.24%) | Memory: 16.6 MB (66.98%) - LeetSync

--- 805-escape-the-ghosts/escape-the-ghosts.py
+++ 805-escape-the-ghosts/escape-the-ghosts.py
@@ -2,7 +2,6 @@ class Solution:
     def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
         a=abs(target[0])+abs(target[1])
         for i ,j in ghosts:
-            # b=
             if abs(target[0]-i)+abs(target[1]-j)<=a:
                 return False
         return True
