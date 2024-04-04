0537023a3477b2b54bf1c97ac0f6fe48037caeb6
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-04-04 11:08:12 +0300

    Time: 147 ms (59.70%) | Memory: 17.1 MB (81.05%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
@@ -1,21 +1,43 @@
+# class Solution:
+#     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+     
+#         store= defaultdict(list)
+#         if len(list1)>len(list2):
+#             list1,list2=  list2, list1
+        
+#         for i, num in enumerate(list1):
+#             Total=0
+#             if num in list2:
+#                 Total= i + list2.index(num)
+#                 store[Total].append(num)  
+        
+#         final=min(store.items())
+#         return final[1]
+
+
+
+
+
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-     
-        store= defaultdict(list)
-        if len(list1)>len(list2):
-            list1,list2=  list2, list1
+        if len(list1) > len(list2):
+            list1, list2 = list2, list1
         
-        for i, num in enumerate(list1):
-            Total=0
-            if num in list2:
-                Total= i + list2.index(num)
-                store[Total].append(num)  
-        
-        final=min(store.items())
-        return final[1]
+        hashmap = {}
+        for i, word in enumerate(list1): 
+            hashmap[word] = i
+
+        min_index_sum = float("inf")
+        for i, word in enumerate(list2):
+            if word in hashmap: # if we have a common word
+                curr_sum = hashmap[word] + i
+                if curr_sum < min_index_sum:
+                    min_index_sum = curr_sum
+                    answer = [word]
+                elif curr_sum == min_index_sum:
+                    answer.append(word)
+
+        return answer
 
 
-
-
-
-        
+                
