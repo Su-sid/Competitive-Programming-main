61992570701a84e1cbd1a114a0243c5efe6e2069
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-04-16 22:33:56 +0300

    Time: 298 ms (40.40%) | Memory: 32 MB (83.27%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -1,17 +1,42 @@
 #Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution:
+#     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+#         nums= []
+#         while head:
+#             nums.append(head.val)
+#             head= head.next
+#         if nums== nums[::-1]:
+#             return True
+#         else:
+#             return False
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        nums= []
-        while head:
-            nums.append(head.val)
-            head= head.next
-        if nums== nums[::-1]:
-            return True
-        else:
-            return False
+        fast=slow= head
+        while fast and fast.next:
+            fast=fast.next.next
+            slow= slow.next
+
+        #reverse the second half
+
+        prev =None
+        while slow:
+            tmp= slow.next
+            slow.next=prev
+            prev=slow
+            slow=tmp
+
+        #check palindrome
+        left, right= head, prev
+        while right:  # use prev pointer for the second half
+            if left.val != right.val:
+                return False 
+            left = left.next 
+            right = right.next
+        return True
+
 
         
