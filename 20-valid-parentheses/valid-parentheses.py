100c015ae38dc802db4c2d606f50a550ab714636
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-04-22 21:31:09 +0300

    Time: 32 ms (77.44%) | Memory: 16.5 MB (74.83%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if s[0] in (')', '}', ']'):
+            return False
+        stack = []
+        par = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        for paran in s:
+            if paran in ('(', '[', '{'):
+                stack.append(paran)
+
+            elif stack and par[paran] == stack[-1]: 
+                stack.pop()
+
+            else:
+                return False 
+
+        return len(stack) == 0
