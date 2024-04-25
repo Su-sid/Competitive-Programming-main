5eee6f1c204b6570c0064f947e2038197690c456
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-04-25 11:56:42 +0300

    Time: 50 ms (5.43%) | Memory: 16.7 MB (14.23%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
@@ -5,20 +5,41 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#using pointers approach
+        # dummy = ListNode()
+        # dummy.next = head
+        # prev = dummy
+        # cur = head
+        # while cur and cur.next:
 
-        dummy = ListNode()
-        dummy.next = head
-        prev = dummy
-        cur = head
-        while cur and cur.next:
+        #     if cur.next.val == cur.val:
+        #         while cur.next and cur.next.val == cur.val:
+        #             cur = cur.next
+        #         cur.next, cur = None, cur.next
+        #         prev.next = cur
+        #     else:
+        #         prev = cur
+        #         cur = cur.next
 
-            if cur.next.val == cur.val:
-                while cur.next and cur.next.val == cur.val:
-                    cur = cur.next
-                cur.next, cur = None, cur.next
-                prev.next = cur
-            else:
-                prev = cur
-                cur = cur.next
+        # return dummy.next
+#using hashmap
+        # def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        a=[]
+        cur=head
+        while cur:
+            a.append(cur.val)
+            cur=cur.next
+        dic=Counter(a)
+        ans=[]
+        for i,j in dic.items():
+            if j==1:
+                ans.append(i)
+        p=ListNode(0)
+        k=p
+        for i in range(len(ans)):
+            l=ListNode(ans[i])
+            p.next=l
+            p=p.next
+        return k.next
 
-        return dummy.next
+
