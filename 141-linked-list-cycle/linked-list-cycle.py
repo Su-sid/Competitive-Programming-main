9636942f4ee43afa45c35cb01817e5c612919dc9
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-04-30 18:38:47 +0300

    Time: 54 ms (11.19%) | Memory: 19.1 MB (33.00%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -4,30 +4,26 @@
 #         self.val = x
 #         self.next = None
 
-# class Solution:
-#     def hasCycle(self, head: Optional[ListNode]) -> bool:
-#         slow, fast= head, head 
-
-#         # if head is None:
-#         #     return False
-
-#         while fast and fast.next:
-#             slow= slow.next
-#             fast=  fast.next.next
-            
-#             if slow== fast:
-#                 # print(fast)
-#                 return True 
-#         return False 
-        
-
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        hashmap= set()
-        while head:
-            if head in hashmap:
-                return True 
-            hashmap.add(head)
-            head =head.next
-        return False 
-        
+        slow, fast= head, head
+
+
+        while fast and fast.next:
+            slow= slow.next
+            fast=  fast.next.next
+
+            if fast==slow:
+                # print(fast)
+                return True
+        return False
+
+# class Solution:
+#     def hasCycle(self, head: Optional[ListNode]) -> bool:
+#         hashmap = set()
+#         while head:
+#             if head in hashmap:
+#                 return True
+#             hashmap.add(head)
+#             head = head.next
+#         return False
