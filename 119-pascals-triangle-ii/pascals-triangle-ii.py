6b1e885e5d6f0e224bda64a6eb8cf9dc061a56f8
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-05-01 21:06:00 +0300

    Time: 37 ms (45.18%) | Memory: 16.5 MB (37.72%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -1,14 +1,21 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
 
-        row = [1] * (rowIndex + 1)
-        for i in range(1, rowIndex):
-            print(f'row for {i} is {row}')
-            for j in range(i, 0, -1):
-                row[j] += row[j - 1]
+        # row = [1] * (rowIndex + 1)
+        # for i in range(1, rowIndex):
+        #     print(f'row for {i} is {row}')
+        #     for j in range(i, 0, -1):
+        #         row[j] += row[j - 1]
               
-        return row
+        # return row
 
-        
-        
-        
+        if rowIndex == 0:
+            return [1] 
+        prev = self.getRow(rowIndex-1)
+        row = [1]
+
+        for i in range(1, len(prev)):
+            row.append(prev[i-1] + prev[i])
+
+        row.append(1)
+        return row        
