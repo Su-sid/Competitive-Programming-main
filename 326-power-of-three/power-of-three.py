1fab1e34062b52a2b3e9111cc81a82025fa5a551
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-05-01 16:42:45 +0300

    Time: 1094 ms (5.05%) | Memory: 16.6 MB (23.40%) - LeetSync

---
+++ 326-power-of-three/power-of-three.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+
+        #base case
+        if n==1:
+            return True
+
+        #recursive relation
+        if n<=0:
+            return False
+
+        #function call 
+        return self.isPowerOfThree(n/3)
