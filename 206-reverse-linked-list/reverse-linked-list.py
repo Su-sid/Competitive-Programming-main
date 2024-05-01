a173cb33dfd93a5f5519b09c767f023dfd4bd3cf
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-05-01 19:15:56 +0300

    Time: 38 ms (50.84%) | Memory: 18.3 MB (5.78%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -6,32 +6,27 @@
 class Solution:
 
     def __init__(self):
-        self.new_list= None
+        self.new_list = None
+
+    def reverser(self, current):
+        if current == None:
+            return self.new_list
+
+        if current != None:
+            next_node = current.next
+            current.next = self.new_list
+            self.new_list = current
+            current = next_node
+
+        return self.reverseList(current)
 
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-       
-        current=head
-        final=self.reverser(current)
+        current = head
+        final = self.reverser(current)
         return final
-
         # while current:
         #     next_node= current.next
         #     current.next= new_list
         #     new_list=current
         #     current=next_node
         # return new_list
-
-    def reverser(self, current):
-        if current == None:
-            return self.new_list
-        # else:
-        if current != None:
-            next_node= current.next
-            current.next= self.new_list
-            self.new_list=current
-            current=next_node
-        # current= next_node
-        return self.reverseList(current)
-
-
-            
