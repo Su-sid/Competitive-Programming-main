83401cf9e428502e242deed71f0fb63d34c6d4d0
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-05-06 14:26:57 +0300

    Time: 39 ms (30.74%) | Memory: 16.5 MB (97.13%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -1,30 +1,34 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        # even =[0,2,4,6,8]
 
-        # odd=[2, 3, 5, 7]
         MOD= 10**9 +7
 
-        count=1 
+        # count=1 
 
-        if n % 2 ==0:
-            count*= pow(4,(n//2),MOD)
+        # if n % 2 ==0:
+        #     count*= pow(4,(n//2),MOD)
 
-            count*= pow(5,(n//2),MOD)
+        #     count*= pow(5,(n//2),MOD)
 
             
-            count%= MOD
+        #     count%= MOD
             
-        else:
-            count*= pow(4,(n//2),MOD)
+        # else:
+        #     count*= pow(4,(n//2),MOD)
 
-            count*= pow(5,(n+1)//2,MOD)
+        #     count*= pow(5,(n+1)//2,MOD)
 
-            count%=MOD
-            print(count)
-            
-        return count
-            
+        #     count%=MOD
+        #     print(count)
+
+        # return count
+        
+        odd=n//2
+
+        even=n//2+n%2
+
+        return (pow(5, n//2+n%2, MOD)  * pow(4,(n//2), MOD)) % MOD
+            #$ ((pow(5,even,mod)*pow(4,odd, mod))%mod)        
            
 
 
