7e71cae6c4ca3f51596f5956095c19e94a6f0ad1
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-05-06 14:27:48 +0300

    Time: 34 ms (67.74%) | Memory: 16.6 MB (72.64%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -2,7 +2,11 @@ class Solution:
     def countGoodNumbers(self, n: int) -> int:
 
         MOD= 10**9 +7
+        odd=n//2
+        even=n//2+n%2
 
+        return (pow(5, n//2+n%2, MOD)  * pow(4,(n//2), MOD)) % MOD
+        # MOD= 10**9 +7
         # count=1 
 
         # if n % 2 ==0:
@@ -22,49 +26,3 @@ class Solution:
         #     print(count)
 
         # return count
-        
-        odd=n//2
-
-        even=n//2+n%2
-
-        return (pow(5, n//2+n%2, MOD)  * pow(4,(n//2), MOD)) % MOD
-            #$ ((pow(5,even,mod)*pow(4,odd, mod))%mod)        
-           
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # def is_prime(n):
-        #     if n <= 1:
-        #         return False
-        #     for i in range(2, (n**n) + 1):
-        #         if n % i == 0:
-        #             return False
-        #     return True
