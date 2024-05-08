5730510cadc301a6e55b136406ad5070f31a6c10
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-05-08 12:05:44 +0300

    Time: 36 ms (48.60%) | Memory: 16.5 MB (96.73%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,9 +1,11 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-
+        
+        #condition
         if x!=0 or n>0:
             return x**n
 
+        #recursive state
         return self.myPow(x,n-1)
 
 
