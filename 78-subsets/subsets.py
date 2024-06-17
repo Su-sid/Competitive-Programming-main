17d61804eb0693049d5bee478e4e89db33836e8a
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-06-17 12:16:34 +0300

    Time: 34 ms (77.06%) | Memory: 16.6 MB (71.83%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,34 +1,34 @@
 class Solution:
-    # def subsets(self, nums: List[int]) -> List[List[int]]:
-        #create the recursive function
-        # def create_subset(i):
-        #     if i == len(nums):
-        #         res.append(subset[:])
-        #         return
-            
-        #     subset.append(nums[i])
-        #     create_subset(i+1)
-
-        #     subset.pop()
-        #     create_subset(i+1)
-
-        # res, subset= [], []
-        # create_subset(0)
-        # return res
-
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res, path = [], []
-        self.dfs(0, res, path, nums)
-        return res
+        # create the recursive function
+        def create_subset(i):
+            if i == len(nums):
+                res.append(subset[:])
+                return
             
-    def dfs(self, index, res, path, nums):
-        res.append(list(path))     # 
+            subset.append(nums[i])
+            create_subset(i+1)
 
-        for i in range(index, len(nums)):
+            subset.pop()
+            create_subset(i+1)
 
-            path.append(nums[i])
-            self.dfs(i+1, res, path, nums)
-            print(path)
-            # break
-            path.pop()
+        res, subset= [], []
+        create_subset(0)
+        return res
+
+    # def subsets(self, nums: List[int]) -> List[List[int]]:
+    #     res, path = [], []
+    #     self.dfs(0, res, path, nums)
+    #     return res
+            
+    # def dfs(self, index, res, path, nums):
+    #     res.append(list(path))     # 
+
+    #     for i in range(index, len(nums)):
+
+    #         path.append(nums[i])
+    #         self.dfs(i+1, res, path, nums)
+    #         print(path)
+    #         # break
+    #         path.pop()
     
