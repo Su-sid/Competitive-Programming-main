4aabc4b3df85f442b24ead8349f2f1a4f8a845a1
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-06-23 12:37:04 +0300

    Time: 45 ms (79.08%) | Memory: 16.4 MB (90.91%) - LeetSync

---
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.py
@@ -0,0 +1,26 @@
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+       
+        def dfs(index):
+            if index >= len(cookies):
+                self.best_distribution = min(self.best_distribution, max(children_cookies))
+                return
+          
+            # Iterate through each child
+            for j in range(k):
+                if children_cookies[j] + cookies[index] >= self.best_distribution or (j > 0 and children_cookies[j] == children_cookies[j - 1]):
+                    continue
+              
+                # Distribute current cookie to child j and recurse
+                children_cookies[j] += cookies[index]
+                dfs(index + 1)
+                children_cookies[j] -= cookies[index]
+   
+        self.best_distribution = float('inf')
+       
+        children_cookies = [0] * k
+
+        cookies.sort(reverse=True)
+  
+        dfs(0)
+        return self.best_distribution
