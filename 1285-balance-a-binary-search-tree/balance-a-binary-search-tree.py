ff2492627c2711e059ba093882d09e4a57ebb43e
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-06-23 11:56:10 +0300

    Time: 205 ms (44.11%) | Memory: 22 MB (46.26%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.py
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.py
@@ -6,13 +6,8 @@
 #         self.right = right
 class Solution:
     def balanceBST(self, root: TreeNode) -> TreeNode:
-        # arr=[]
-
-        # create a arr of all nodes
         vector=[]
-
         def dfs(root):
-
             if not root:
                 return
             val= root.val
@@ -20,17 +15,8 @@ class Solution:
             vector.append(val)
             dfs(root.right)
         dfs(root)
-        print(vector)
-        
-        # - tree ( convert to ) vector
-        # - sort the vector
-        # - sorted vector -> Inorder Traversal
-        # - that\'s all
-        # create a arr of all nodes
-
         def tCreate(l,r):
             node = TreeNode()
-            # print(node)
             if l> r:
                 return 
             mid = (l+r)//2
@@ -38,7 +24,5 @@ class Solution:
             node.val= vector[mid]
             node.left= tCreate(l,mid-1)
             node.right=tCreate(mid+1,r)       
-
             return node
-
         return tCreate(0,len(vector)-1)
