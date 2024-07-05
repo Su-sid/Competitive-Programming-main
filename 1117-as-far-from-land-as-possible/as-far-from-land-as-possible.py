e891d8536fc10df43296a23cf68e62c09f008a92
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-07-05 16:44:39 +0300

    Time: 399 ms (63.56%) | Memory: 17.1 MB (81.07%) - LeetSync

--- 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.py
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.py
@@ -5,23 +5,17 @@ class Solution:
         queue = deque((i,j) for i in range(row) for j in range(col) if grid [i][j]  )  
          
         ans=-1
-        #create the ans gridrix
-        
-
-        # print(queue)
-
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)] #back,down,up,front 
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)] 
 
         def boundChecker(nx, ny):
             return 0 <= nx < row and 0 <= ny < col
-
+            
         if len(queue) in (0, row * row):
             return ans      
 
         while queue:
             ans+=1
             for _ in range(len(queue)):
-                # i, j = queue.popleft()
                 x, y = queue.popleft()
                 for dx, dy in directions:
                     nx, ny = x + dx, y + dy
