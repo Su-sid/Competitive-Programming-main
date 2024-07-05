685a2030e46e1db7e0827949a9db4be4a4bfa54a
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-07-05 16:41:21 +0300

    Time: 390 ms (67.51%) | Memory: 17.3 MB (53.00%) - LeetSync

---
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.py
@@ -0,0 +1,34 @@
+class Solution:
+    def maxDistance(self, grid: List[List[int]]) -> int:
+       
+        row, col = len(grid), len(grid[0])
+        queue = deque((i,j) for i in range(row) for j in range(col) if grid [i][j]  )  
+         
+        ans=-1
+        #create the ans gridrix
+        
+
+        # print(queue)
+
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)] #back,down,up,front 
+
+        def boundChecker(nx, ny):
+            return 0 <= nx < row and 0 <= ny < col
+
+        if len(queue) in (0, row * row):
+            return ans      
+
+        while queue:
+            ans+=1
+            for _ in range(len(queue)):
+                # i, j = queue.popleft()
+                x, y = queue.popleft()
+                for dx, dy in directions:
+                    nx, ny = x + dx, y + dy
+                    
+                    if boundChecker(nx,ny) and grid[nx][ny] == 0:
+                        grid[nx][ny]=1 
+                        queue.append((nx, ny))
+                
+        
+        return ans
