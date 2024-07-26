fbe0225deb60d38646d05044287dca65b2f02b2e
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-07-26 21:16:55 +0300

    Time: 360 ms (83.40%) | Memory: 16.8 MB (93.00%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,25 +1,22 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        
-        # where X is the frequency of occurrence of  the most frequent element.
+        '''
+        Scenario 1: (n+1)*(f-1) + X.
+        Note: Where X is the frequency of occurrence of  the most frequent element.
+        Final  scenario: max ( (n+1)*(f-1) + X, TotalJobs)
+        '''
         TotalJobs= len(tasks)
         f=Counter(tasks).most_common()
         maxFreq= f[0][1]
 
         X= 0
+        # Count number of occurence of  most frequent character.
+        
         for element, freq in  f:
             if freq == maxFreq:
                 X+=1
+            else: 
+                break
         
-        # print(maxFreq)
-        # print(X)
-
-        # print(f)
 
         return max ( (n+1)*(maxFreq-1) + X, TotalJobs)
-
-        '''
-        (n+1)*(f-1) + X.
-
-        max ( (n+1)*(f-1) + X, TotalJobs)
-        '''
