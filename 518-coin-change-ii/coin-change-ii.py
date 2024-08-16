5c9e081f4ea3b0070fff07c7bb63606bf16a6c8f
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-08-16 21:35:44 +0300

    Time: 1949 ms (6.42%) | Memory: 26.3 MB (55.45%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        def dp(memo, n, start):
+            if n == 0:
+                return 1
+            if memo[n][start] != -1:
+                return memo[n][start]
+            
+            memo[n][start] = 0
+            
+            for i in range(start, len(coins)):
+                coin = coins[i]
+                if n - coin >= 0:
+                    memo[n][start] += dp(memo, n - coin, i)
+            
+            return memo[n][start]
+
+        # def coinChangeII(amount, coins):
+        memo = [[-1] * len(coins) for _ in range(amount + 1)]
+        return dp(memo, amount, 0)
