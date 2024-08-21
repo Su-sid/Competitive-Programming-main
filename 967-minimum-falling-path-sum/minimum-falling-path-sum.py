f525daa40f509fde7c5c8d56c25f26090463ad0a
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-08-21 21:20:45 +0300

    Time: 114 ms (96.28%) | Memory: 17.4 MB (46.29%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -6,7 +6,9 @@ class Solution:
         for row in range(1, n):
             for col in range(n):
                 left= matrix [row -1 ] [col-1] if col > 0 else float('inf') 
+
                 right = matrix [row -1 ] [col+1] if col < n-1 else float('inf') 
+                
                 middle= matrix [row -1 ] [col] 
 
                 matrix[row][col]=  matrix[row][col]+ min(left, right, middle)
