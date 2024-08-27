1c5ccbd62803215ab712089ae810ea33a654ce64
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-08-27 14:01:09 +0300

    Time: 205 ms (26.01%) | Memory: 19.2 MB (12.67%) - LeetSync

--- 1582-design-browser-history/design-browser-history.py
+++ 1582-design-browser-history/design-browser-history.py
@@ -1,33 +1,39 @@
-
-class Node:
-    def __init__(self, val=0, next=None, prev=None):
-        self.val = val
-        self.next = next
-        self.prev = prev
-        
 class BrowserHistory:
-#use a double linked list
+    # use two stacks one for keeping the forward history and the primary one to keep backward history. 
+    # Once you reach a back function cal, pop from backHistory stack and append popped value to forwardHistory stack. 
+    # Once you reach a forward function cal, pop from forwardHistory stack and append popped value to backHistory stack. 
+
     def __init__(self, homepage: str):
-        self.head = Node(homepage)
-        self.curr = self.head
+        self.backHistory, self.forwardHistory= [], []
+        self.visit(homepage)
 
     def visit(self, url: str) -> None:
-        node = Node(url)
-        self.curr.next = node
-        node.prev = self.curr
-        self.curr = node
-        
+        self.backHistory.append(url)
+        self.forwardHistory= []
+
     def back(self, steps: int) -> str:
-        while self.curr.prev and steps:
-            self.curr = self.curr.prev
-            steps -= 1
-        return self.curr.val
+        # while len of backHistory stack is greater than 1 and steps is not == 0 pop from backHistory and append popped value to forwardHistory stack. 
+      
+        while len(self.backHistory) > 1 and steps:
+            temp= self.backHistory.pop()
+
+            self.forwardHistory.append(temp)
+            steps-=1
+           
+        return self.backHistory[-1]
+        
 
     def forward(self, steps: int) -> str:
-        while self.curr.next and steps:
-            self.curr = self.curr.next
-            steps -= 1
-        return self.curr.val
+    #    while stack is not empty and steps is not == 0 execute the while statement. 
+        while self.forwardHistory and steps:
+            temp= self.forwardHistory.pop()
+
+            self.backHistory.append(temp)
+            steps-=1
+
+        # return the last/ top element of the backHistory stack
+        return self.backHistory[-1]
+
         
 
 
@@ -35,10 +41,4 @@ class BrowserHistory:
 # obj = BrowserHistory(homepage)
 # obj.visit(url)
 # param_2 = obj.back(steps)
-# param_3 = obj.forward(steps)
-
-# Your BrowserHistory object will be instantiated and called as such:
-# obj = BrowserHistory(homepage)
-# obj.visit(url)
-# param_2 = obj.back(steps)
-# param_3 = obj.forward(steps)
+# param_3 = obj.forwardHistoryd(steps)
