729b0e0760f83ea834df78981b04924798656c47
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-09-25 11:19:07 +0300

    Time: 396 ms (65.21%) | Memory: 22.6 MB (9.25%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -1,20 +1,46 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        maxWeight, totalWeight = -1, 0
-      
-        left, right = max(weights), sum(weights)
+                
+        def binarySearch(array):
+            # left, right = 0 , len(array)-1
+            left, right = max(array), sum(array)
 
-        while left < right:
-            mid = (left + right) // 2
+            minAnswer = sum(array)
 
-            daysNeeded, currWeight = 1, 0
-            for weight in weights:
-                if currWeight + weight > mid:
-                    daysNeeded += 1
-                    currWeight = 0
-                currWeight += weight
-            if daysNeeded > days:
-                left = mid + 1
-            else:
-                right = mid
-        return left
+            while left<= right:
+                
+                capacity = left+ (right- left)//2
+
+                if possible(capacity):
+                    minAnswer= min(minAnswer, capacity) 
+                    right= capacity-1
+                else:
+                    left= capacity +1
+
+            return minAnswer
+
+        # returns true if and only if we can do the task in D days.
+        def possible(capacity: int) -> bool:
+            cusum=0
+            ships =1
+
+            for num in weights:
+                # if num > capacity: return False
+
+                if cusum + num > capacity:
+
+                    cusum= num
+                    ships+=1
+                    # if ships> days:
+                    #     return False
+                else:
+                    cusum+=num
+            return ships <= days
+
+        return binarySearch(weights)
+
+
+
+
+
+
