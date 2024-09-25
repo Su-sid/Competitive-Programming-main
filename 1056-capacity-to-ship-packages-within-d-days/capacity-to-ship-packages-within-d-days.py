9cca236f40345753d346b1e9a03faee46fcf17e0
Sudi <62776074+Su-sid@users.noreply.github.com>
2024-09-25 12:00:47 +0300

    Time: 411 ms (44.28%) | Memory: 22.3 MB (81.71%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -2,11 +2,8 @@ class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
                 
         def binarySearch(array):
-            # left, right = 0 , len(array)-1
-            left, right = max(array), sum(array)
-
+            left, right =0, sum(array)
             minAnswer = sum(array)
-
             while left<= right:
                 
                 capacity = left+ (right- left)//2
@@ -19,28 +16,18 @@ class Solution:
 
             return minAnswer
 
-        # returns true if and only if we can do the task in D days.
         def possible(capacity: int) -> bool:
             cusum=0
             ships =1
 
             for num in weights:
-                # if num > capacity: return False
-
+                if num > capacity:
+                    return False
                 if cusum + num > capacity:
-
                     cusum= num
                     ships+=1
-                    # if ships> days:
-                    #     return False
                 else:
                     cusum+=num
             return ships <= days
 
         return binarySearch(weights)
-
-
-
-
-
-
