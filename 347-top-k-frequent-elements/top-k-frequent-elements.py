83fd21ac3fa6f44ae44f0366020df0e2c0a2868b
David Sudi <62776074+Su-sid@users.noreply.github.com>
2024-10-11 22:36:51 +0300

    Time: 95 ms (24.77%) | Memory: 21.3 MB (34.17%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,10 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    
+        store = Counter(nums)
+
+        sorted_store = sorted(store.items(), key=lambda x: x[1], reverse=True)
+
+        final = [key for key, _ in sorted_store[:k]]
+
+        return final
