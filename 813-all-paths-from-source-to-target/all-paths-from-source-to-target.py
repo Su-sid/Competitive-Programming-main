ffb4e6b52578260ba4999abc4f354688b3ab375f
David Sudi <62776074+Su-sid@users.noreply.github.com>
2024-11-05 11:10:32 +0300

    Time: 8 ms (17.92%) | Memory: 18 MB (84.73%) - LeetSync

---
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -0,0 +1,35 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        graphStore = defaultdict(list)
+        visited =set()
+        tempArr=list()
+        finalArr=list()
+        destination= len(graph)-1
+
+        for node, children in enumerate(graph):
+            # if not children:
+            #     destination= node 
+            graphStore[node]=children
+
+        print(destination)
+            
+        def dfs(node):
+            visited.add(node)
+            tempArr.append(node)
+
+            if node == destination:
+                finalArr.append(tempArr[:])
+          
+            for neighbor in graphStore[node]:
+                if neighbor not in visited:
+                    dfs(neighbor)
+
+            visited.remove(node)
+            tempArr.pop()
+
+        dfs(0)
+
+        return  finalArr
+
+                
+
