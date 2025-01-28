3dd694026cab6c7559ed0a04cb2b836bfb49f29b
David Sudi <62776074+Su-sid@users.noreply.github.com>
2025-01-28 10:41:23 +0300

    Time: 3 ms (16.40%) | Memory: 17.8 MB (25.57%) - LeetSync

---
+++ 258-add-digits/add-digits.py
@@ -0,0 +1,17 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+      
+        def rec(num):
+            strNum = [int(i) for i in str(num)] 
+            if len(strNum) <2:
+                num = strNum[0]
+                print(num)
+                return num
+            return rec(sum(strNum))
+
+        return rec(num)
+            
+
+            
+            
+        
