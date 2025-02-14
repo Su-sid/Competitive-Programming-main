b83d54ce28bc77e7c30a6d375696b0475c13fa71
David Sudi <62776074+Su-sid@users.noreply.github.com>
2025-02-14 11:52:09 +0300

    Time: 49 ms (87.45%) | Memory: 17.7 MB (55.37%) - LeetSync

--- 3463-alternating-groups-i/alternating-groups-i.py
+++ 3463-alternating-groups-i/alternating-groups-i.py
@@ -3,6 +3,9 @@ class Solution:
         Acount=0
         n =len(colors)
 
+        if n<3:
+            return 0
+
         for i in range(n):
             if colors[i%n]== colors[(i+2)%n] and colors[i%n] !=colors[(i+1) %n]:
                 Acount+=1
