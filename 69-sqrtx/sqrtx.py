01e4645ece98afb611379851ab8ca1d3b149de14
David Sudi <62776074+Su-sid@users.noreply.github.com>
2025-10-02 23:02:05 +0300

    Time: 3 ms (60.01%) | Memory: 17.9 MB (10.76%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,26 +1,20 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+
+        left, right= 0, x
         
-        
+        while left <= right:
+        # while left <=right:
+            mid=left+ (right-left)//2
+            # print("left",left,"right",right,"mid", mid)
+            if mid * mid == x:
+                return mid
+            if mid * mid < x:
+                left= mid+1
+            # elif mid * mid > x :
+            else:
+                right= mid-1
 
-        def rec(x):
-            left, right= 0, x
-            
-            while left <=right:
-                mid=(left+ (right-left)//2)
-                # print("left",left,"right",right,"mid", mid)
-                if mid * mid == x:
-                    return mid
-                if mid * mid < x:
-                    left= mid+1
-                # elif mid * mid > x :
-                else:
-                    right= mid-1
+            # return mid
+        return right
 
-                # return mid
-            return right
-
-
-        return rec(x)
-        # return 0
-        
