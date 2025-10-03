d60797a2d13fdc8a9d84dc7afebc36d769e0ca53
David Sudi <62776074+Su-sid@users.noreply.github.com>
2025-10-03 13:19:55 +0300

    Time: 11 ms (67.61%) | Memory: 17.8 MB (64.23%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.py
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -1,15 +1,31 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        x, y = 0, 0
+        # x, y = 0, 0
 
-        for i in moves:
-            if i == "U":
-                y +=1
-            if i == "D":
-                y -=1
-            if i == "R":
-                x +=1
-            if i == "L":
-                x -=1
+        # for i in moves:
+        #     if i == "U":
+        #         y +=1
+        #     if i == "D":
+        #         y -=1
+        #     if i == "R":
+        #         x +=1
+        #     if i == "L":
+        #         x -=1
         
-        return ( x == 0 and y == 0)
+        # return ( x == 0 and y == 0)
+
+        '''
+        you can also use a Counter to check the number of moves of each direction you are stepping to 
+
+        - For instance, given the directions U, D if the count is equal between the two pairs then it can be safely
+        deduced that the robot moved back to its original position
+        '''
+
+        from collections import Counter
+
+        moves_count = Counter(moves)
+
+        print(moves_count)
+
+        return (moves_count.get('U', 0) == moves_count.get('D', 0) and moves_count.get('L', 0) == moves_count.get('R', 0))
+
